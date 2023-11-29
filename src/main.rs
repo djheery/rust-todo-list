6f915673837b14cb836f91b086102559dcf87142
Daniel Heery <danieljheery@gmail.com>
2023-11-29 18:26:49 +0000

    Refactoring how the TodoList item is created/ added too.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#![allow(unused)]
 
 #[derive(Debug)]
 struct TodoListItem {
@@ -5,6 +6,47 @@ struct TodoListItem {
   item_title: String, 
   item_description: String,
   item_completed: bool, 
+}
+
+#[derive(Debug)]
+enum ItemUpdateFields {
+    TITLE,
+    DESCRIPTION,
+}
+
+impl TodoListItem {
+
+    fn new(id: String, item_title: String, item_description: String, item_completed: bool) -> TodoListItem {
+      TodoListItem {
+        id, 
+        item_title, 
+        item_description, 
+        item_completed
+      }
+    }
+
+    fn print_item_title(&self) {
+        println!("> Title: {}", self.item_title); 
+    }
+
+    fn print_item_description(&self) {
+        println!("> Descripton: {}", self.item_description); 
+    }
+
+    fn print_item_completion_status(&self) {
+        println!("> Is Completed: {}", self.item_completed.to_string());
+    }
+
+    fn set_item_details(&mut self, field: ItemUpdateFields, update: String) {
+       match field {
+         ItemUpdateFields::TITLE => self.item_title = update, 
+         ItemUpdateFields::DESCRIPTION => self.item_description = update, 
+       }
+    }
+
+    fn set_item_completion_status(&mut self, update: bool) {
+       self.item_completed = update;  
+    }
 }
 
 #[derive(Debug)]
@@ -17,37 +59,35 @@ struct TodoList {
 impl TodoList {
     fn new(todo_list_items: Vec<TodoListItem>, title: String, id: String) -> TodoList {
         TodoList {
-           id: id, 
+           id, 
            todo_list_title: title, 
-           todo_list_items: todo_list_items, 
-        }
+           todo_list_items, 
+     }
     }
 
     fn get_item_at_index(&self, index: usize) {
         let item = self.todo_list_items.get(index);  
     }
 
-    fn update_item_at_index(&mut self, index: usize, new_item: TodoListItem) {
-       let item = self.todo_list_items.get(index);  
+    fn add_new_tdl_item(&mut self, item: TodoListItem) {
+        self.todo_list_items.push(item);
+    }
+    
+    fn remove_item_at_index(&mut self, index: usize) {
+        
     }
 
-    fn update_item_description(item: &mut TodoListItem, item_description: String) {
-       item.item_description = item_description; 
-    }
-
-    fn update_item_title(item: &mut TodoListItem, item_title: String) {
-        item.item_title = item_title; 
-    }
-
-    fn update_completed_status(item: &mut TodoListItem, completed_status: bool) {
-       item.item_completed = completed_status; 
+    fn update_item_at_index(&mut self, index: usize) {
+        
     }
 }
 
 fn main() {
-    let todo_list: Vec<TodoListItem> = create_tdl_item(); 
-    print_menu(&todo_list); 
-    show_full_item_at_index(&todo_list, 2) 
+    let todo_list_items: Vec<TodoListItem> = create_tdl_item(); 
+    let title = String::from("My Super TodoList"); 
+    let id = String::from("123456"); 
+    let todo_list = TodoList::new(todo_list_items, title, id);
+
 }
 
 fn print_menu(todo_list: &Vec<TodoListItem>) {
